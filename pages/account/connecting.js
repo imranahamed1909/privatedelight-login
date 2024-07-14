9ff2a79d1c9592084ad25ef26b11a1d6e9b7f38b
Zayn Yusuf <yusue786@gmail.com>
2024-07-14 23:02:08 +0600

    change font

--- pages/account/connecting.js
+++ pages/account/connecting.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Connecting() {
-  return <div className="mt-10 font-2xl font-semibold">Connecting....</div>;
+  return (
+    <div className="mt-10 text-center font-2xl font-base">Connecting....</div>
+  );
 }
